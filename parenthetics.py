74b13a53b18935289cb6ced94e345179c9556475
Mark Saiget <saiget@gmail.com>
2015-02-07 14:08:09 -0800

    Remove recursive method and replace with more efficient method.

--- parenthetics.py
+++ parenthetics.py
@@ -1,29 +1,24 @@
-"""Method to determine if string contains proper parenthetics.
-Returns 1 if string is 'open', not all parentheses have been closed.
-Returns 0 if string is balanced.
-Returns -1 if string is 'broken', a closing parens has not been preceed by an
-    open parentese.
-"""
 import stack as st
 
 
 def parenthetics(input_string):
-    only_parens = st.Stack()
+    """Method that will test string for proper parenthetics.
+    Returns 1 if string is 'open', not all parentheses have been closed.
+    Returns 0 if string is balanced.
+    Returns -1 if string is 'broken', a closing parens has not been preceeded
+    by an open parentese.
+    """
+    open_parens = st.Stack()
     for letter in input_string:
-        if letter == '(' or letter == ')':
-            only_parens.push(letter)
-    out = check_parens(only_parens)
-    # Reduce values greater than 1 to 1 and less than -1 to -1
-    if out != 0:
-        out = out / abs(out)
-    return out
-
-
-def check_parens(input_stack):
-    if input_stack.top:
-        parens = input_stack.pop()
-        if parens == '(':
-            return check_parens(input_stack) + 1
-        if parens == ')':
-            return check_parens(input_stack) - 1
-    return 0
+        # add all open parens to stack
+        if letter == '(':
+            open_parens.push(letter)
+        # check that for every lose parens there is an open in the stack
+        if letter == ')':
+            if open_parens.top:
+                if open_parens.pop() == '(':
+                    continue
+            return -1   # if no open parens in stack
+    if open_parens.top:     # if extra open parens
+        return 1
+    return 0        # parens must be balanced.
