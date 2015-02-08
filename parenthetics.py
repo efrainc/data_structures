814d56ae762b6954ca0a4a8459a883be26253e3d
efrainc <efrainc88@gmail.com>
2015-02-08 14:47:50 -0800

    Completed test and parenthetics files. Tested for all logical string combinations.Used string encoding in main function to capture utf-8

--- parenthetics.py
+++ parenthetics.py
@@ -3,5 +3,17 @@
 # Efrain Camacho Parenthetics Assignmetn
 
 def paren_checker(text):
-    return None
+    """Checks to confirm each opening paren is closed correcrly."""
+    encoded_string = text.encode('utf-8')
+    count = 0
+    for i in encoded_string:
+        if i == ')':
+            count -= 1
+        elif i == '(':
+            count += 1
+        if count < 0:
+            return -1
+    if count > 1:
+            return 1
+    return 0
 
--- test_parenthetics.py
+++ test_parenthetics.py
@@ -9,23 +9,25 @@ def test_function():
     """Test various configuration of paraenthesis using pc function"""
     strings_container = random_strings()
     assert pc(strings_container[0]) == 0
+    assert pc(strings_container[1]) == 0
+    assert pc(strings_container[2]) == 1
+    assert pc(strings_container[3]) == -1
+    assert pc(strings_container[4]) == -1
 
     return None
-
-
-
-
-# Return 1 if the string is "open" (there are open parens that are not closed)
-# Return 0 if the string is "balanced" (there are an equal number of open and closed parentheses in the string)
-# Return -1 if the string is "broken" (a closing parens has not been proceeded by one th
-
 
 @pytest.fixture(scope='function')
 def random_strings():
     """Fixture contains random strings for testing."""
 
-    a = '(abcd)'
-    b = '((D))()'
-    c = '(((())'
-    d = ')))((('
-    return a, b, c, d
+    string_a = '(abcd)'
+    string_b = '((D))()'
+    string_c = '(((())'
+    string_d = ')))((('
+    string_e = '())('
+    return string_a, string_b, string_c, string_d, string_e
+
+#Testing guidelines
+# Return 1 if the string is "open" (there are open parens that are not closed)
+# Return 0 if the string is "balanced" (there are an equal number of open and closed parentheses in the string)
+# Return -1 if the string is "broken" (a closing parens has not been proceeded by one that opens)
