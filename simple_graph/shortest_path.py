ace28383b70e593ee20651dc0486d71c2643ba0b
Mark Saiget <saiget@gmail.com>
2015-03-03 16:52:59 -0800

    Add dijkstraPQ method.

--- simple_graph/shortest_path.py
+++ simple_graph/shortest_path.py
@@ -38,15 +38,26 @@ def dijkstra(weighted_graph, start, end):
     return list_of_tuples_node_totalweight[-1][1]
 
 
+def dijkstraPQ(weighted_graph, start, end):
+    """Dijkstra algorithm with PQ."""
+    dict_node_totalweight = {}
+    dict_node_totalweight[start] = 0
+    prev = []
+    pq = PriorityQueue()
+    for node in weighted_graph.nodes():
+        if node is not start:
+            dict_node_totalweight[node] = float("inf")
+        pq.put((float("inf"), node))
 
-    # already_visited = [start]
-    # for node in weighted_graph:
-    #     if node is not start:
-    #         weight = 100
-    #         # previous = undefined
-    #     pq = Pq.insert(node, weight)
-
-    # while Pq:
-    #     temp = pq.pop()
-    #     for neighbor in weighted_graph.neighbors(temp):
-    #         alt =
+    while pq:
+        temp = pq.get()
+        if temp[1] == end:
+            break
+        for neighbor in weighted_graph.neighbors(temp[1]):
+            alt = temp[0] + weighted_graph.dict[temp[1]][neighbor]
+            if alt < dict_node_totalweight[neighbor]:
+                dict_node_totalweight[neighbor] = alt
+                prev.append(temp[0])
+                pq.put(alt, neighbor)
+    print 'prev: {}'.format(prev)
+    return prev
